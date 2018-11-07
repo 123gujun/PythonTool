5cc19e4d47fa71334e2f67a41ed55019aae54e85
gujun <gujun.123@163.com>
2018-11-07 14:20:21 +0800

    complete uploading hawkeye face recognition

--- upload.py
+++ upload.py
@@ -1,21 +1,24 @@
 import requests
 import os
-url = "http://192.168.1.188:88/verify/face/add"
+url = "http://192.168.10.240/api/personnels"
 
-querystring = {"dbName":"test1234"}
+querystring = {"group_id": 1}
+
 filesPath = "D:\\WorkSpace\\2k\\"
 for imagePath in os.listdir(filesPath):
     if imagePath.split(".")[1]=="jpg" or imagePath.split(".")[1]=="png":
         fullPath = filesPath + imagePath
         print(fullPath)
         files = {
-            'imageData': open(fullPath, 'rb')
+            'photo': open(fullPath, 'rb'),
+            'name':(None,imagePath),
+            'group_id':(None,str(1))
         }
         headers = {
             'Cache-Control': "no-cache",
             'Postman-Token': "75b7c49d-6c6d-43ba-ad19-70f1b84043b2"
             }
 
-        response = requests.request("POST", url, files=files, headers=headers, params=querystring)
+        response = requests.request("POST", url, files=files, headers=headers)
 
         print(response.text)

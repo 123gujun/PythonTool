dd7d56f27bd8b8dff543d734e0a2b42aa179fb1c
123gujun <gujun.123@163.com>
2019-01-09 11:42:28 +0800

    modify split dir

--- imageFile.py
+++ imageFile.py
@@ -6,7 +6,7 @@ import base64
 import json,time
 
 
-    f=open("D:\\WorkSpace\\1w\\VT001_00000061.jpg",'rb')
+f=open("D:\\WorkSpace\\1w\\VT001_00000061.jpg",'rb')
 img=f.read()
 base64_data=base64.b64encode(img).decode('utf-8')
 f.close()
--- splitDir.py
+++ splitDir.py
@@ -2,14 +2,14 @@
 import os
 import shutil
 
-filesPath = u"D:\\WorkSpace\\1w\\"
+filesPath = u"D:\\WorkSpace\\20W\\"
 def  getFiles(filesPath):
-    '''获取5000文件'''
+    '''获取5w文件'''
     fullPath = []
     for imagePath in os.listdir(filesPath):
         if imagePath.split(".")[1]=="jpg" or imagePath.split(".")[1]=="png":
             print(len(fullPath))
-            if len(fullPath) < 10000:
+            if len(fullPath) < 50000:
                 fullPath.append(filesPath + imagePath)
             else:
                 return fullPath
@@ -19,14 +19,15 @@ files = getFiles(filesPath)
 filecount =0
 pathcount = 1
 
+
 path = filesPath + "image" + str(pathcount)
 os.mkdir(path)
 for file in files:
-    if filecount < 1000:
+    if filecount < 10000:
         shutil.copy(file,path)
         filecount += 1
     else:
-        if pathcount >= 10:
+        if pathcount > 5:
             break
         else:
             pathcount += 1
